e71c2ab575930bd07cc2f707357d06baef1a4786
GokulSankar <78421362+Gokul-gok@users.noreply.github.com>
2026-06-04 14:30:15 +0100

    changes

--- src/sqoop_import.sh
+++ src/sqoop_import.sh
@@ -5,7 +5,7 @@ sqoop import \
   --connect 'jdbc:postgresql://13.42.152.118:5432/testdb' \
   --username admin --password admin123 \
   --table dim_date \
-  --target-dir /tmp/yamini/tfl_project1/dim_date \
+  --target-dir /tmp/gokul/tfl_project1/dim_date \
   --num-mappers 1 \
   --fields-terminated-by ',' \
   --delete-target-dir
@@ -15,7 +15,7 @@ sqoop import \
   --connect 'jdbc:postgresql://13.42.152.118:5432/testdb' \
   --username admin --password admin123 \
   --table dim_stations \
-  --target-dir /tmp/yamini/tfl_project1/dim_stations \
+  --target-dir /tmp/gokul/tfl_project1/dim_stations \
   --num-mappers 1 \
   --fields-terminated-by ',' \
   --delete-target-dir
@@ -25,7 +25,7 @@ sqoop import \
   --connect 'jdbc:postgresql://13.42.152.118:5432/testdb' \
   --username admin --password admin123 \
   --table dim_networks \
-  --target-dir /tmp/yamini/tfl_project1/dim_networks \
+  --target-dir /tmp/gokul/tfl_project1/dim_networks \
   --num-mappers 1 \
   --fields-terminated-by ',' \
   --delete-target-dir
@@ -35,7 +35,7 @@ sqoop import \
   --connect 'jdbc:postgresql://13.42.152.118:5432/testdb' \
   --username admin --password admin123 \
   --table dim_lines \
-  --target-dir /tmp/yamini/tfl_project1/dim_lines \
+  --target-dir /tmp/gokul/tfl_project1/dim_lines \
   --num-mappers 1 \
   --fields-terminated-by ',' \
   --delete-target-dir
@@ -45,7 +45,7 @@ sqoop import \
   --connect 'jdbc:postgresql://13.42.152.118:5432/testdb' \
   --username admin --password admin123 \
   --table fact_station_lines \
-  --target-dir /tmp/yamini/tfl_project1/fact_station_lines \
+  --target-dir /tmp/gokul/tfl_project1/fact_station_lines \
   --num-mappers 1 \
   --fields-terminated-by ',' \
   --delete-target-dir
@@ -55,7 +55,7 @@ sqoop import \
   --connect 'jdbc:postgresql://13.42.152.118:5432/testdb' \
   --username admin --password admin123 \
   --table fact_passenger_entry_exit \
-  --target-dir /tmp/yamini/tfl_project1/fact_passenger_entry_exit \
+  --target-dir /tmp/gokul/tfl_project1/fact_passenger_entry_exit \
   --num-mappers 1 \
   --fields-terminated-by ',' \
   --delete-target-dir
